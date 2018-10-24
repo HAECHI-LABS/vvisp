32729e6eb8f4c54a131fdb919d1fb0f6f875925d
JhChoy <jchoyeclipse@gmail.com>
2018-10-24 12:11:27 +0900

    fix abi-to-script

--- test/scripts/abi-to-script/index.test.js
+++ test/scripts/abi-to-script/index.test.js
@@ -17,9 +17,9 @@ describe('# abi-to-script process test', function() {
   before(function() {
     fs.removeSync(ROOT);
   });
-  after(function() {
-    fs.removeSync(ROOT);
-  });
+  // after(function() {
+  //   fs.removeSync(ROOT);
+  // });
   describe('# back version', function() {
     describe('# process test', function() {
       before(function() {
@@ -82,65 +82,70 @@ describe('# abi-to-script process test', function() {
         hasIndex.should.be.equal(true);
       });
     });
-    // describe('# output test', function() {
-    //
-    // });
-  });
-  describe('# front version', function() {
-    describe('# process test', function() {
-      before(function() {
-        this.files = [CONTRACT1, CONTRACT2];
-        this.name = 'haechi';
-        this.root = path.join(ROOT, 'front');
-        this.abi = path.join(this.root, 'abi');
-        this.js = path.join(this.root, 'js');
+    describe('# output test', function() {
+      before(async function() {
+        this.apis = require('../../../contractApis/back');
       });
-      it('should be fulfilled', async function() {
-        await abiToScript(this.files, { silent: true, front: this.name }).should
-          .be.fulfilled;
-      });
-      it('should make all directories', function() {
-        fs.lstatSync(ROOT)
-          .isDirectory()
-          .should.be.equal(true);
-        fs.lstatSync(this.root)
-          .isDirectory()
-          .should.be.equal(true);
-        fs.lstatSync(this.abi)
-          .isDirectory()
-          .should.be.equal(true);
-        fs.lstatSync(this.js)
-          .isDirectory()
-          .should.be.equal(true);
-      });
-      it('should make all abi files', function() {
-        const files = fs.readdirSync(this.abi);
-        files.length.should.be.equal(this.files.length);
-        for (let i = 0; i < files.length; i++) {
-          path.parse(files[i]).ext.should.be.equal('.json');
-        }
-      });
-      it('should make all js files', function() {
-        const files = fs.readdirSync(this.js);
-        files.length.should.be.equal(this.files.length + 1); // with index file
-        let hasIndex = false;
-        for (let i = 0; i < files.length; i++) {
-          const parses = path.parse(files[i]);
-          parses.ext.should.be.equal('.js');
-          if (parses.name === this.name) {
-            hasIndex = true;
-          }
-        }
-        hasIndex.should.be.equal(true);
-      });
-      it('should make rollup files', function() {
-        fs.lstatSync(path.join(this.root, 'haechi.js'))
-          .isFile()
-          .should.be.equal(true);
-        fs.lstatSync(path.join(this.root, 'haechi.es.js'))
-          .isFile()
-          .should.be.equal(true);
+      it('should get two constructors', function() {
+        console.log(this.apis);
       });
     });
   });
+  // describe('# front version', function() {
+  //   describe('# process test', function() {
+  //     before(function() {
+  //       this.files = [CONTRACT1, CONTRACT2];
+  //       this.name = 'haechi';
+  //       this.root = path.join(ROOT, 'front');
+  //       this.abi = path.join(this.root, 'abi');
+  //       this.js = path.join(this.root, 'js');
+  //     });
+  //     it('should be fulfilled', async function() {
+  //       await abiToScript(this.files, { silent: true, front: this.name }).should
+  //         .be.fulfilled;
+  //     });
+  //     it('should make all directories', function() {
+  //       fs.lstatSync(ROOT)
+  //         .isDirectory()
+  //         .should.be.equal(true);
+  //       fs.lstatSync(this.root)
+  //         .isDirectory()
+  //         .should.be.equal(true);
+  //       fs.lstatSync(this.abi)
+  //         .isDirectory()
+  //         .should.be.equal(true);
+  //       fs.lstatSync(this.js)
+  //         .isDirectory()
+  //         .should.be.equal(true);
+  //     });
+  //     it('should make all abi files', function() {
+  //       const files = fs.readdirSync(this.abi);
+  //       files.length.should.be.equal(this.files.length);
+  //       for (let i = 0; i < files.length; i++) {
+  //         path.parse(files[i]).ext.should.be.equal('.json');
+  //       }
+  //     });
+  //     it('should make all js files', function() {
+  //       const files = fs.readdirSync(this.js);
+  //       files.length.should.be.equal(this.files.length + 1); // with index file
+  //       let hasIndex = false;
+  //       for (let i = 0; i < files.length; i++) {
+  //         const parses = path.parse(files[i]);
+  //         parses.ext.should.be.equal('.js');
+  //         if (parses.name === this.name) {
+  //           hasIndex = true;
+  //         }
+  //       }
+  //       hasIndex.should.be.equal(true);
+  //     });
+  //     it('should make rollup files', function() {
+  //       fs.lstatSync(path.join(this.root, 'haechi.js'))
+  //         .isFile()
+  //         .should.be.equal(true);
+  //       fs.lstatSync(path.join(this.root, 'haechi.es.js'))
+  //         .isFile()
+  //         .should.be.equal(true);
+  //     });
+  //   });
+  // });
 });
