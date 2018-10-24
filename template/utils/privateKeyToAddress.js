f7522e989c6da10b3c2409e03ecce2be5f98a611
JhChoy <jchoyeclipse@gmail.com>
2018-10-24 23:44:20 +0900

    update template utils

--- template/utils/privateKeyToAddress.js
+++ template/utils/privateKeyToAddress.js
@@ -1,9 +1,33 @@
-module.exports = function(_privKey) {
+module.exports = function(privateKey) {
+  privateKey = checkInput(privateKey);
   const web3 = require('./getWeb3')();
 
-  if (_privKey[0] + _privKey[1] !== '0x') {
-    _privKey = '0x' + _privKey;
-  }
+  return web3.eth.accounts.privateKeyToAccount(privateKey).address;
 
-  return web3.eth.accounts.privateKeyToAccount(_privKey).address;
+  function checkInput(privateKey) {
+    if (privateKey === undefined || null) {
+      throw new TypeError(`Input privateKey is ${privateKey}`);
+    } else if (typeof privateKey === 'string') {
+      if (privateKey[0] + privateKey[1] !== '0x') {
+        privateKey = '0x' + privateKey;
+      }
+      if (privateKey.length !== 66) {
+        throw new TypeError(`Input privateKey has wrong length`);
+      }
+    } else if (Buffer.isBuffer(privateKey)) {
+      if (privateKey.length === 32) {
+        privateKey = '0x' + privateKey.toString('hex');
+      } else if (privateKey.length === 64) {
+        privateKey = '0x' + privateKey.toString();
+      } else {
+        throw new TypeError(
+          'Wrong Buffer length! Expected 32 or 64 instead of ' +
+            privateKey.length
+        );
+      }
+    } else {
+      throw new TypeError('Wrong Type! privateKey should be string or Buffer');
+    }
+    return privateKey;
+  }
 };
