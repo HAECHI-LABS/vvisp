0923de68fe40624ebe772ead20d2cd99b74bf10f
JhChoy <jchoyeclipse@gmail.com>
2019-01-11 22:21:42 +0900

    fix: make compile to filter solidity files

--- packages/vvisp/scripts/compile.js
+++ packages/vvisp/scripts/compile.js
@@ -11,7 +11,9 @@ module.exports = async function(files, options) {
   fs.ensureDirSync(path.join('build', 'contracts'));
 
   if (files.length === 0) {
-    files = getAllFiles(path.join('./', 'contracts'));
+    files = getAllFiles(path.join('./', 'contracts'), filePath => {
+      return path.parse(filePath).ext === '.sol';
+    });
   }
 
   const output = await compile(files, options ? options.silent : undefined);
@@ -19,6 +21,7 @@ module.exports = async function(files, options) {
   const contractNames = Object.keys(output.contracts);
   const contractContents = Object.values(output.contracts);
 
+  // TODO: output file refine
   for (let i = 0; i < contractNames.length; i++) {
     const contractName = path.parse(contractNames[i]).name;
     contractContents[i].contractName = contractName;
