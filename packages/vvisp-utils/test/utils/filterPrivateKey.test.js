7a52e14f35e0b969275ec0610b1e5017591d469a
JhChoy <jchoyeclipse@gmail.com>
2019-01-24 12:52:55 +0900

    test: refine test
    remove compile log
    remove warning at test
    remove unnecessary test code in deploy-contract.test.js

--- packages/vvisp-utils/test/utils/filterPrivateKey.test.js
+++ packages/vvisp-utils/test/utils/filterPrivateKey.test.js
@@ -6,10 +6,11 @@ require('dotenv').config();
 const { getPrivateKey } = require('../../src');
 const filterPrivateKey = require('../../src/utils/filterPrivateKey');
 
-describe('# privateKeyToAddress test', function() {
+describe('# filterPrivateKey test', function() {
   before(function() {
     this.privateKey = getPrivateKey(process.env.MNEMONIC);
   });
+
   describe('# input arguments', function() {
     it('should reject when private is wrong type', function() {
       expect(() => filterPrivateKey(undefined)).to.throw(TypeError);
@@ -18,23 +19,26 @@ describe('# privateKeyToAddress test', function() {
       expect(() => filterPrivateKey({ a: 1 })).to.throw(TypeError);
       expect(() => filterPrivateKey(['hello', 'there'])).to.throw(TypeError);
     });
+
     it("should reject when string private's length is not 64 or 66(with 0x)", function() {
       expect(() => filterPrivateKey(this.privateKey + 'd')).to.throw(TypeError);
       expect(() => filterPrivateKey('d125541543514123')).to.throw(TypeError);
     });
+
     it("should reject when Buffer private's length is not 32 or 64", function() {
-      const shortBuffer1 = new Buffer(this.privateKey.slice(6));
-      const shortBuffer2 = new Buffer(this.privateKey.slice(6), 'hex');
-      const longBuffer1 = new Buffer(this.privateKey + '32');
-      const longBuffer2 = new Buffer(this.privateKey + '32', 'hex');
+      const shortBuffer1 = Buffer.from(this.privateKey.slice(6));
+      const shortBuffer2 = Buffer.from(this.privateKey.slice(6), 'hex');
+      const longBuffer1 = Buffer.from(this.privateKey + '32');
+      const longBuffer2 = Buffer.from(this.privateKey + '32', 'hex');
       expect(() => filterPrivateKey(shortBuffer1)).to.throw(TypeError);
       expect(() => filterPrivateKey(shortBuffer2)).to.throw(TypeError);
       expect(() => filterPrivateKey(longBuffer1)).to.throw(TypeError);
       expect(() => filterPrivateKey(longBuffer2)).to.throw(TypeError);
     });
+
     it('should receive right type', function() {
-      const buffer32 = new Buffer(this.privateKey, 'hex');
-      const buffer64 = new Buffer(this.privateKey);
+      const buffer32 = Buffer.from(this.privateKey, 'hex');
+      const buffer64 = Buffer.from(this.privateKey);
       expect(() => filterPrivateKey(buffer32)).to.not.throw();
       expect(() => filterPrivateKey(buffer64)).to.not.throw();
       expect(() => filterPrivateKey(this.privateKey)).to.not.throw();
