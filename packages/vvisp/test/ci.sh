969cc00be4c7f8f1c6f01175eed409afccbbe868
Youngtaek Oh <oyt311@hanmail.net>
2019-01-25 19:12:03 +0900

    ci: fix err state

--- packages/vvisp/test/ci.sh
+++ packages/vvisp/test/ci.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
+set -o errexit
+
 cp test/dummy/test.env .
 mv test.env .env
 
 nyc --reporter=html --reporter=text mocha $(find ./test -name '*.test.js') --recursive
-
 nyc report --reporter=text-lcov | coveralls
