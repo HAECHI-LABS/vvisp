1e0ea3cc0b5bb702f2526340ddf0e3ce852d5bcd
JhChoy <jchoyeclipse@gmail.com>
2019-02-18 19:05:21 +0900

    fix: example test case convention, fix package.json

--- packages/vvisp/referenceFiles/test/Example.test.js
+++ packages/vvisp/referenceFiles/test/Example.test.js
@@ -1,14 +1,19 @@
-const { BN } = require('openzeppelin-test-helpers');
+const { BN, shouldFail, constants } = require('openzeppelin-test-helpers');
 
-const Contract = artifacts.require('Contract');
+const ContractFactory = artifacts.require('Contract');
 
 contract('Contract', accounts => {
   const [owner] = accounts;
 
-  beforeEach(async function() {
-    this.contract = await Contract.new({ from: owner });
+  let Contract;
+
+  beforeEach(async () => {
+    Contract = await ContractFactory.new({ from: owner });
   });
-  it('should be a example test', async function() {
-    await this.contract.exampleFunction();
+
+  describe('#exampleFunction()', () => {
+    it('should be a example test', async () => {
+      await Contract.exampleFunction();
+    });
   });
 });
