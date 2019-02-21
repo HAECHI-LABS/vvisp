4dbfed53daa51e947283d8ca42f49aa106f82ad6
JhChoy <jchoyeclipse@gmail.com>
2019-02-21 21:08:22 +0900

    fix: fix getConfigRoot function async to sync

--- packages/vvisp-utils/src/getConfigRoot.js
+++ packages/vvisp-utils/src/getConfigRoot.js
@@ -1,8 +1,8 @@
-module.exports = async function(configFileName) {
+module.exports = function(configFileName) {
   const findUp = require('find-up');
   const path = require('path');
 
-  let configPath = await findUp([configFileName]);
+  let configPath = findUp.sync([configFileName]);
   if (!configPath) {
     throw new Error(`${configFileName} is not found`);
   }
