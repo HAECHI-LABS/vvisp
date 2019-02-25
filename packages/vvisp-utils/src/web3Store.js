8efa917e78d6a2c3ca562cce2fe83de176b501ff
JhChoy <jchoyeclipse@gmail.com>
2019-02-25 14:48:10 +0900

    fix: change getWeb3 to web3Store

---
+++ packages/vvisp-utils/src/web3Store.js
@@ -0,0 +1,35 @@
+const Web3 = require('web3');
+
+let web3;
+const voidWeb3 = new Web3();
+module.exports = (function() {
+  return {
+    /**
+    @dev setWithProvider set web3 with provider
+     */
+    setWithProvider: provider => {
+      web3 = new Web3(provider);
+    },
+    /**
+    @dev setWithURL set web3 with http url
+     */
+    setWithURL: url => {
+      web3 = new Web3(new Web3.providers.HttpProvider(url));
+    },
+    /**
+    @dev get getter of stored web3, if there isn't, return void web3
+     */
+    get: () => {
+      if (!web3) {
+        return voidWeb3;
+      }
+      return web3;
+    },
+    /**
+    @dev delete delete stored web3 for test
+     */
+    delete: () => {
+      web3 = undefined;
+    }
+  };
+})();
