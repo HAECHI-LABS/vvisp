019424595afc2feb32363f321addfa76ab8990b4
JhChoy <jchoyeclipse@gmail.com>
2019-02-25 14:50:20 +0900

    ci: make generate vvisp-config and ignore some files

--- packages/vvisp-utils/test/ci.sh
+++ packages/vvisp-utils/test/ci.sh
@@ -4,6 +4,8 @@ set -o errexit
 
 cp test/dummy/test.env .
 mv test.env .env
+cp test/dummy/sample.vvisp-config.js .
+mv sample.vvisp-config.js vvisp-config.js
 
 nyc --reporter=html --reporter=text mocha $(find ./test -name '*.test.js') --recursive
 nyc report --reporter=text-lcov | coveralls
