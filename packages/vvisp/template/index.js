3edc2ad955443ec5fa3ba6fddc69543f86c3e09c
JhChoy <jchoyeclipse@gmail.com>
2019-02-25 15:09:09 +0900

    feat: apply Config and web3Store at generated js scripts

--- packages/vvisp/template/index.js
+++ packages/vvisp/template/index.js
@@ -1,8 +1,29 @@
 const fs = require('fs');
 const path = require('path');
+const { Config, web3Store } = require('@haechi-labs/vvisp-utils');
 
 const files = fs.readdirSync(path.join(__dirname, 'js'));
-const apis = {};
+const apis = {
+  config: configObj => {
+    let config;
+    try {
+      config = Config.get(configObj);
+    } catch (e) {}
+    if (!config) {
+      config = new Config();
+      config.merge(configObj);
+    }
+    Config.setStore(config);
+  },
+  setWeb3: web3Setter => {
+    if (web3Setter && typeof web3Setter === 'string') {
+      web3Store.setWithURL(web3Setter);
+    } else if (web3Setter) {
+      web3Store.setWithProvider(web3Setter);
+    }
+  }
+};
+
 for (let i = 0; i < files.length; i++) {
   if (files[i].slice(-3) === '.js') {
     if (files[i] === 'index.js') {
@@ -12,4 +33,8 @@ for (let i = 0; i < files.length; i++) {
   }
 }
 
-module.exports = apis;
+module.exports = function(configOption, web3Setter) {
+  apis.config(configOption);
+  apis.setWeb3(web3Setter);
+  return apis;
+};
