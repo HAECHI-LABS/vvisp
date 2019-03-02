e7c1f19b19e8a01fc0a773cff7a89adaa61bb8e9
JChoy <jchoyeclipse@gmail.com>
2019-03-02 19:28:39 +0900

    fix: change default config options (#106)

--- packages/vvisp-utils/test/dummy/sample.vvisp-config.js
+++ packages/vvisp-utils/test/dummy/sample.vvisp-config.js
@@ -19,7 +19,7 @@ module.exports = {
       settings: {
         // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
-          enabled: false,
+          enabled: true,
           runs: 200
         },
         evmVersion: 'byzantium'
