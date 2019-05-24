987f4ed1a239159662ae1aeb586f6f263de42ad7
chiwon <chiwon@chiwons-MacBook-Pro.local>
2019-05-24 06:36:25 -0700

    coverage test

--- packages/vvisp/scripts/coverage.js
+++ packages/vvisp/scripts/coverage.js
@@ -1,3 +1,37 @@
-function printTest(){
-    printOrSilent('this is test');
-}
+module.exports = async function(files, options) {
+    options = require('./utils/injectConfig')(options);
+  
+    const path = require('path');
+    const fs = require('fs-extra');
+    const {
+      compile,
+      getAllFiles,
+      printOrSilent
+    } = require('@haechi-labs/vvisp-utils');
+  
+    fs.ensureDirSync(path.join('./', 'build'));
+    fs.ensureDirSync(path.join('build', 'contracts'));
+  
+    if (files.length === 0) {
+      files = getAllFiles(path.join('./', 'contracts'), filePath => {
+        return path.parse(filePath).ext === '.sol';
+      });
+    }
+  
+    const output = await compile(files, options);
+  
+    // TODO: output file refine
+    Object.entries(output.contracts).forEach(
+      ([contractNamePath, contractContent]) => {
+        const contractName = path.parse(contractNamePath).name;
+        contractContent.contractName = contractName;
+        fs.writeJsonSync(
+          path.join('./build/contracts', contractName + '.json'),
+          contractContent,
+          { spaces: '  ' }
+        );
+      }
+    );
+    printOrSilent('Compiling Finished!', options);
+  };
+  
