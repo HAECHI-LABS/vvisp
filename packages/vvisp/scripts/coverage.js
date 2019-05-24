8bc52b4a6bfba932146c0286ef51440eb2dd47d0
chiwon <chiwon@chiwons-MacBook-Pro.local>
2019-05-24 11:23:50 -0700

    coverage test

--- packages/vvisp/scripts/coverage.js
+++ packages/vvisp/scripts/coverage.js
@@ -1,6 +1,9 @@
 module.exports = async function(files, options) {
     options = require('./utils/injectConfig')(options);
+    const path = require('path');
     const { getAllFiles, printOrSilent } = require('@haechi-labs/vvisp-utils');
+
+    printOrSilent(path);
     printOrSilent('Compiling Finished!', options);
   };
   
