de1aaa5dcb929166edff9f301b599878cd752f41
chiwon <chiwon@chiwons-MacBook-Pro.local>
2019-05-24 19:46:44 -0700

    coverage test

--- packages/vvisp/scripts/coverage.js
+++ packages/vvisp/scripts/coverage.js
@@ -10,11 +10,14 @@ module.exports = async function(files, options) {
     }
 
     path.join(rootDir,'scripts/coverage.sh');
-    exec(rootDir,(error,stdout,stderr));
+    exec(rootDir,(error,stdout,stderr) => {
     if(error) {
         console.error('exec error: ${error}');
         return;
     }
+    console.log('stdout: ${stdout}');
+    console.log('stderr: ${stderr}');
+    });
     printOrSilent('Compiling Finished!', options);
   };
   
