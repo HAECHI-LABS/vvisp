47fb0e1fa8de8110579c6f9b0deb986ef80adfef
chiwon <chiwon@chiwons-MacBook-Pro.local>
2019-05-24 20:43:33 -0700

    add coverage command finished

--- packages/vvisp/scripts/coverage.js
+++ packages/vvisp/scripts/coverage.js
@@ -1,24 +1,15 @@
-module.exports = async function(files, options) {
-    options = require('./utils/injectConfig')(options);
-    const path = require('path');
+module.exports = async function() {
     const { printOrSilent } = require('@haechi-labs/vvisp-utils');
-    const { exec } = require('child_process');
-    const process = require('process');
-    /*
-    let rootDir = path.join('./');
-    if (options && options.directory) {
-      rootDir = path.join(options.directory);
-    }
-*/
-    console.log(process.execPath);
-    //exec('sh scripts/coverage.sh',(error,stdout,stderr) => {
-    //f(error) {
-    //    console.error('exec error: ${error}');
-    //    return;
-    //}
-    //console.log('stdout:'+ stdout);
-    //console.log('stderr:'+ stderr);
-    //});
-    printOrSilent('Compiling Finished!', options);
+
+    const exec = require('child_process').exec;
+    const testscript = exec('sh scripts/coverage.sh')
+    
+    testscript.stdout.on('data', function(data){
+       console.log(data);
+    });
+    testscript.stderr.on('data', function(data){
+        console.log(data);
+    });
+    
 }
   
