6dba8cca7d3fcf62c4876bf14f83b3693cc85dbd
JhChoy <jchoyeclipse@gmail.com>
2019-06-14 17:11:01 +0900

    fix: fix bug about array arguments at deploy-contract

--- packages/vvisp/scripts/deploy-contract.js
+++ packages/vvisp/scripts/deploy-contract.js
@@ -14,6 +14,21 @@ module.exports = async function(file, arguments, options) {
     gasLimit: options.config.gasLimit
   };
 
+  arguments = arguments.map(arg => {
+    // convert array string to array
+    // "[0x123, 0x234]" to ["0x123", "0x234"]
+    if (arg.startsWith('[') && arg.endsWith(']')) {
+      return arg
+        .slice(1, arg.length - 1)
+        .split(/\s*,\s*/)
+        .map(v => {
+          // remove empty space for each element
+          return v.trim();
+        });
+    }
+    return arg;
+  });
+
   await compileAndDeploy(file, privateKey, arguments, options);
 
   printOrSilent('Deploying Finished!', options);
