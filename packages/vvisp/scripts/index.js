e1fedf316588500525d2072530e877dbdbd7de50
wetcod <iscc3208@gmail.com>
2019-06-18 17:20:20 +0900

    fix: split pull reqeust

--- packages/vvisp/scripts/index.js
+++ packages/vvisp/scripts/index.js
@@ -4,12 +4,7 @@ const deployService = require('./deploy-service');
 const deployContract = require('./deploy-contract');
 const init = require('./init');
 const flatten = require('./flatten');
-const debug = require('./debug');
 const console = require('./console');
-const showState = require('./show-state');
-const test = require('./test');
-const analyze = require('./analyze');
-const ci = require('./ci');
 
 module.exports = {
   genScript,
@@ -18,10 +13,5 @@ module.exports = {
   deployContract,
   init,
   flatten,
-  debug,
-  console,
-  showState,
-  test,
-  analyze,
-  ci
+  console
 };
--- packages/vvisp/test/ssci.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -o errexit
-
-cp test/dummy/sample.vvisp-config.js .
-mv sample.vvisp-config.js vvisp-config.js
-
-nyc --reporter=html --reporter=text mocha $(find ./test/scripts/show-state -name '*.test.js') --recursive
-nyc report --reporter=text-lcov | coveralls
