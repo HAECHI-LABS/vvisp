3d4bfac6029e96c94d1e8fff8bb60aeb5c40fdbd
wetcod <iscc3208@gmail.com>
2019-06-18 17:14:28 +0900

    fix: split pull request

--- packages/vvisp/commands/index.js
+++ packages/vvisp/commands/index.js
@@ -1,28 +1,17 @@
-const analyze = require('./analyze');
-const ci = require('./ci');
 const compile = require('./compile');
 const console = require('./console');
-const test = require('./test')
-const debug = require('./debug');
 const deployContract = require('./deploy-contract');
 const deployService = require('./deploy-service');
 const flatten = require('./flatten');
 const genScript = require('./gen-script.js');
-const showState = require('./show-state');
 const init = require('./init');
 
-
 module.exports = [
-  analyze,
-  ci,
   compile,
   console,
-  debug,
   deployContract,
   deployService,
   flatten,
   genScript,
-  init,
-  showState,
-  test
+  init
 ];
