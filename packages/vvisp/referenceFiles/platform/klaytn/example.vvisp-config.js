0d7ebde939136aa7d37f8fbad16572172f3f107f
JhChoy <jchoyeclipse@gmail.com>
2019-06-25 01:25:13 +0900

    fix: add .gitattribute, add comments, update chore things in ref (#180)

--- packages/vvisp/referenceFiles/platform/klaytn/example.vvisp-config.js
+++ packages/vvisp/referenceFiles/platform/klaytn/example.vvisp-config.js
@@ -8,7 +8,9 @@ externalConfig.networks.baobab = {
 };
 
 module.exports = {
+  // network: baobab, // If you want to use baobab network, uncomment this line.
   ...externalConfig,
+  // or from: '0x9741fa712a6912b862c9043f8752ffae513cb01895985998c61620da5aaf2d2d' // private Key
   from: {
     mnemonic: MNEMONIC,
     index: 0
