10c54175e245e31cb929c6710de303220cc672fe
JhChoy <jchoyeclipse@gmail.com>
2019-06-24 16:01:40 +0900

    test: delete unused test.env and improve getSTDInput.test.js

--- packages/vvisp-utils/test/getSTDInput.test.js
+++ packages/vvisp-utils/test/getSTDInput.test.js
@@ -3,6 +3,7 @@ chai.use(require('chai-as-promised')).should();
 
 const { getSTDInput } = require('../src');
 const bddStdin = require('bdd-stdin');
+const stdMocks = require('std-mocks');
 
 describe('# getSTDInput test', function() {
   it('should read one line.', async function() {
@@ -10,5 +11,17 @@ describe('# getSTDInput test', function() {
     bddStdin(input);
     const line = await getSTDInput();
     line.should.be.equal(input);
+  });
+
+  it('should also print question', async function() {
+    const input = 'hello';
+    const question = 'hello?: ';
+    stdMocks.use();
+    bddStdin(input);
+    const line = await getSTDInput(question);
+    line.should.be.equal(input);
+    stdMocks.restore();
+    const output = stdMocks.flush();
+    output.stdout[0].should.be.equal(question);
   });
 });
