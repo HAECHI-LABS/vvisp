d2c139005573c291251bdac746b96a2d59c8198d
wetcod <iscc3208@gmail.com>
2019-07-01 14:36:43 +0900

    feat: add show-state feature for dynamic variable

--- packages/vvisp/scripts/show-state/index.js
+++ packages/vvisp/scripts/show-state/index.js
@@ -1,18 +1,24 @@
+const VariableTracker = require('./variableTracker');
+const {
+  compile,
+  getLinearContractIds,
+  getContractNodesById,
+  flexTable,
+  addVariableValue
+} = require('./utils');
+const StorageTableBuilder = require('./storageTableBuilder');
+const { printOrSilent } = require('@haechi-labs/vvisp-utils');
 const chalk = require('chalk');
 const fs = require('fs');
 const path = require('path');
-const Web3 = require('web3');
-const { execFileSync } = require('child_process');
-const { compilerSupplier, printOrSilent } = require('@haechi-labs/vvisp-utils');
-const StorageTableBuilder = require('./storageTableBuilder');
-const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545')); // <-----To Do: set real url configured by user
 const stringArgv = require('string-argv');
-var storageTable;
-var address;
-var tableStack = [];
-var storageTableBuilder;
+var web3, address;
+var storageTable, storageTableBuilder;
 
 module.exports = async function(contract, options) {
+  options = require('../utils/injectConfig')(options);
+  web3 = options.web3;
+
   printOrSilent(
     chalk.bold('Now Start Calculating Storage Index of Variables...\n')
   );
@@ -20,24 +26,18 @@ module.exports = async function(contract, options) {
   const vvispState = JSON.parse(fs.readFileSync('./state.vvisp.json', 'utf-8'));
   address = vvispState.contracts[contract].address;
   const srcPath = `./contracts/${vvispState.contracts[contract].fileName}`;
-
   const solcOutput = await compile(srcPath);
-
   const baseAst = solcOutput.sources[srcPath].ast;
   const linearIds = getLinearContractIds(baseAst, contract);
   const nodesById = getContractNodesById(solcOutput);
   const linearNodes = linearIds.map(id => nodesById[id]);
 
+  // build storage table
   storageTableBuilder = new StorageTableBuilder(linearNodes);
+  storageTable = storageTableBuilder.build();
+  storageTable = await addVariableValue(storageTable, address, web3);
 
-  storageTable = storageTableBuilder.build(linearNodes);
-
-  for (let i = 0; i < storageTable.length; i++) {
-    index = storageTable[i][3];
-    value = await web3.eth.getStorageAt(address, index);
-    storageTable[i].push(value);
-  }
-
+  // print table
   printOrSilent(chalk.blue.bold('Contract') + ':' + chalk.bold(contract));
   printOrSilent(
     chalk.blue.bold('Source') + ':' + chalk.bold(path.basename(srcPath))
@@ -45,19 +45,6 @@ module.exports = async function(contract, options) {
   printOrSilent(chalk.blue.bold('Address') + ':' + chalk.bold(address));
   flexTable(storageTable);
   printOrSilent(storageTable.toString());
-
-  /*
-  global.chalk = {
-    success: chalk.green.bold,
-    address: chalk.cyan,
-    tx: chalk.cyan,
-    head: chalk.bold,
-    error: chalk.red.bold,
-    keyWord: chalk.blue.bold,
-    notImportant: chalk.gray,
-    warning: chalk.yellow
-  };
-*/
 
   console.log(
     chalk.bold(
@@ -78,66 +65,6 @@ module.exports = async function(contract, options) {
   await commander.run();
   process.exit(1);
 };
-
-async function compile(srcPath) {
-  // const solcPath = __dirname + '/solc.exe'; // <------To Do: find another way to compile!!
-  // const params = [srcPath, '--combined-json', 'ast,compact-format'];
-  // const options = { encoding: 'utf-8' };
-  // const solcOutput = execFileSync(solcPath, params, options);
-
-  const DEFAULT_COMPILER_VERSION = '0.5.0'; // <------- integrate with compile.js
-
-  const supplier = new compilerSupplier({
-    version: DEFAULT_COMPILER_VERSION
-  });
-  const solc = await supplier.load();
-  const inputDescription = JSON.stringify({
-    language: 'Solidity',
-    sources: {
-      [srcPath]: {
-        content: fs.readFileSync(srcPath, 'utf-8')
-      }
-    },
-    settings: {
-      outputSelection: {
-        '*': {
-          '': ['ast']
-        }
-      }
-    }
-  });
-  const solcOutput = solc.compile(inputDescription);
-
-  fs.writeFileSync('testast.json', solcOutput);
-
-  return JSON.parse(solcOutput);
-}
-
-function getLinearContractIds(ast, targetContract) {
-  return ast.nodes
-    .find(node => node.name == targetContract)
-    .linearizedBaseContracts.reverse();
-}
-
-function getContractNodesById(solcOutput) {
-  return Object.values(solcOutput.sources).reduce((acc, src) => {
-    src.ast.nodes
-      .filter(node => node.nodeType == 'ContractDefinition')
-      .forEach(node => (acc[node.id] = node));
-    return acc;
-  }, {});
-}
-
-function flexTable(table) {
-  table.options.head = table.options.head
-    .map(str => str.toLowerCase())
-    .map(str => chalk.cyanBright.bold(str));
-  table.options.colWidths = [];
-  table.options.chars['left-mid'] = '';
-  table.options.chars['mid'] = '';
-  table.options.chars['right-mid'] = '';
-  table.options.chars['mid-mid'] = '';
-}
 
 function Command(name, options, description, func) {
   return {
@@ -221,234 +148,28 @@ function readLine() {
   });
 }
 
-// == show ==
-// 변수가 존재하면
-// 전부자기자신 그대로 출력
-
-// 동적배열만 자식따라가서 출력? (추가기능)
-
-// 변수가 존재안하면
-// [ 기준으로 잘라냄
-// 앞부분이 없으면 진짜없는거
-// 앞부분이 있으면 타입확인
-// []가 있으면 동적배열 (우선)
-// 우선순위 파싱로직
-// mapping이 있으면 맵핑
-// 우선순위 파싱로직
-// 없으면 잘못된 참조
-
-// 배열 순서는 선언과 거꾸로 된다고 보면됨 (바깥쪽부터 차오르는거는 맞음)
-// 즉 arr[2][4] -> arr[0][0], arr[0][1], arr[1][0], arr[1][1] 순으로 차오름
-// == 우선순위 파싱 로직 ==
-// dim과 Type Flag를 구한다
-// 변수 x[3][key1][key2][3][5][2]
-// dimensions : [3, key1, key2, 3]. 참조하게될 위치
-// type : ( mapping => (mapping => int[2][3][]) )[]
-// typeFlag : ([], mapping, mapping, [], [3], [2]) : 순서대로임
-// 순서대로 참조값이 일치하는지 확인
-//   - dimensions가 더 긴경우 에러
-//   - []인데 key가 들어가면안됨
-// 통과하면 이제 실제로 돌려보면서 값을 구한다.
-//  중간값 처리 x[3]이 child / x가 parent
-//    - x가 동적배열 : x[3]의 인덱스 = 해시(x의 인덱스) + 3
-//    - x가 맵핑 : x[3]의 인덱스 = 해시(키 3+x의 인덱스)
-//    - 일반변수 : 끝
-//    - x가 배열 : x[3]의 인덱스 = x+3
-//    - x가 다중배열 : x[3][3]의 인덱스 = x+9
-//    - x가 구조체 : x.k[]의 인덱스 = x의 구조체테이블안의 k의 인덱스
-//  최종값 처리
-//    - 동적배열 : 현재배열의 자식들 보여줌(선택사항)
-//    - 맵핑 : 그냥 끝
-//    - 일반변수 : 그냥 끝
-//    - 배열 : 그냥 값하나만 보여주자
-//    - 구조체 : 안에있는값들 다보여주자
-
-async function show(args, linearNodes) {
+async function show(args) {
   if (args.length !== 1) {
     console.log(
-      'invalid number of args: should be 1, got {0}'.format(args.length)
+      'invalid nustorageTablember of args: should be 1, got {0}'.format(
+        args.length
+      )
     );
     return;
   }
-  var table = new Table({
-    head: ['VARIABLE', 'TYPE', 'SIZE', 'INDEX', 'STARTBYTE', 'VALUE'],
-    colWidths: [25, 25, 25]
-  });
 
-  // 변수가 존재하면
+  var variableTracker = new VariableTracker(
+    storageTableBuilder.storageTable,
+    web3
+  );
   var name = args[0];
-  if (name in storageTable.get()) {
-    var row = storageTable.get()[name];
-    var type = row[0];
-    // 동적배열이면
-    if (type.indexOf('[]') != -1) {
-      // 따라가서 자식 출력 (추가기능)
-    } else {
-      // 전부자기자신 그대로 출력
-      table.push(row);
-    }
-
-    // 변수가 존재안하면
-  } else {
-    // [ 기준으로 잘라냄
-    name = input.split('[')[0];
-    // 앞부분이 없으면 진짜없는거
-    if (!name in storageTable.get()) {
-      console.log('The variable does not exist.');
-      return -1;
-    } else {
-      // 앞부분이 있으면 타입확인
-      var row = storageTable.get()[name];
-      var type = row[0];
-      // []가 있으면 동적배열 (우선) mapping이 있으면 맵핑
-      if (type.indexOf('[]') != -1 || type.indexOf('mapping') != -1) {
-        // 우선순위대로 따라가보자
-        // == 우선순위 파싱 로직 ==
-        // dim과 Type Flag를 구한다
-        // dimensions : [3, key1, key2, 3]. 참조하게될 위치
-        var getDimensions = new ASTParser().getDimensions;
-        var dimensions = getDimensions(input);
-
-        var typeFlag;
-        /* 계산 규칙 */
-        // type : ( mapping => (mapping => int[2][3][]) )[]
-        // typeFlag : ([], mapping, mapping, [], [3], [2]) : 순서대로임
-
-        // 순서대로 참조값이 일치하는지 확인
-        //   - dimensions가 더 긴경우 에러
-        if (dimensions.length > typeFlag.length) {
-          console.log('Invalid reference.');
-          return -1;
-        }
-
-        //   - 대응되는 놈들이 일반값들이어야함
-        for (var i = 0; i < dimensions.length; i++) {
-          if (typeFlag[i].indexOf('[]') == -1) {
-            if (typeFlag.indexOf('mapping') == -1) {
-              console.log('Invalid reference.');
-              return -1;
-            }
-          } else {
-            //   - []인데 key가 들어가면안됨
-            if (dimensions[i] /* String 이면!! (NaN이면!!)*/) {
-              console.log('Invalid reference.');
-              return -1;
-            }
-          }
-        }
-
-        // 통과하면 이제 실제로 돌려보면서 값을 구한다.
-        child.name = '';
-        parent.name = name;
-        parent.type = type;
-        parent.size = row[1];
-        child.name = child.name + diemnsions[i];
-        child.type = typeFlag[i];
-        for (i = 0; i < dimensions.length; i++) {
-          //  중간값 처리 x[3]이 child / x가 parent
-
-          switch (parent.type) {
-            case darray:
-              //    - x가 동적배열 : x[3]의 인덱스 = 해시(x의 인덱스) + 3
-              var baseIndex = web3.utils.soliditySha3(String(index));
-              baseIndex =
-                '0x' + (BigInt(baseIndex) + BigInt(dimensions[i])).toString(16);
-              break;
-
-            case mapping:
-              //    - x가 맵핑 : x[3]의 인덱스 = 해시(키 3+x의 인덱스)
-              var key = dimensions[i];
-              index = web3.utils.soliditySha3(String(key), String(index));
-              break;
-
-            case normal:
-              //    - 일반변수 : 끝
-              finalData = normal;
-              break;
-
-            case array:
-              //    - x가 배열 : x[3]의 인덱스 = x+3
-              //    - x가 다중배열 : x[3][3]의 인덱스 = x+9
-
-              dimensionsnum = 0;
-
-              for (var j = 0; j < dimensions.length; j++) {
-                if (typeFlag == []) {
-                  break;
-                }
-                dimensionsnum++;
-              }
-              break;
-
-            case struct:
-              //    - x가 구조체 : x.k[]의 인덱스 = x의 구조체테이블안의 k의 인덱스
-              finalData = struct;
-              break;
-          }
-
-          parent.name = child.name;
-          parent.type = child.type;
-          parent.size = row[1];
-          child.name = child.name + diemnsions[i];
-          child.type = typeFlag[i];
-        }
-
-        //  최종값 처리
-
-        switch (child.type) {
-          case darray:
-          //    - 동적배열 : 현재배열의 자식들 보여줌(선택사항)
-
-          case mapping:
-          case normal:
-            //    - 맵핑, 일반변수 : 그냥 끝
-            break;
-
-          case array:
-            //    - 배열 : 그냥 값하나만 보여주자
-
-            break;
-
-          case struct:
-            //    - 구조체 : 안에있는값들 다보여주자
-            break;
-        }
-      } else {
-        // 없으면 잘못된 참조
-        console.log('Invalid reference.');
-        return -1;
-      }
-    }
+  var table = variableTracker.getInfo(name);
+  table = await addVariableValue(table, address, web3);
+  if (table == -1) {
+    return;
   }
 
-  if (array) {
-    var result = await storageTableBuilder.buildDynamicArray(
-      args[0],
-      address,
-      web3,
-      linearNodes
-    );
-    var table = result[0];
-    var baseIndex = result[1];
-
-    for (let i = 0; i < table.length; i++) {
-      value = await web3.eth.getStorageAt(address, baseIndex);
-      table[i].push(value);
-      baseIndex = '0x' + (BigInt(baseIndex) + BigInt(1)).toString(16);
-    }
-  } else if (mapping) {
-    var result = storageTableBuilder.buildMapping(args[0], web3);
-    var table = result[0];
-    var baseIndex = result[1];
-
-    for (let i = 0; i < table.length; i++) {
-      value = await web3.eth.getStorageAt(address, baseIndex);
-      table[i].push(value);
-    }
-  }
-
-  printOrSilent(chalk.blue.bold('variableName') + ':' + chalk.bold(args[0]));
-  printOrSilent(chalk.blue.bold('baseIndex') + ':' + chalk.bold(baseIndex));
+  printOrSilent(chalk.blue.bold('variableName') + ':' + chalk.bold(name));
   flexTable(table);
   printOrSilent(table.toString());
 
---
+++ packages/vvisp/scripts/show-state/utils.js
@@ -0,0 +1,166 @@
+'use strict';
+const { compilerSupplier } = require('@haechi-labs/vvisp-utils');
+const fs = require('fs');
+const chalk = require('chalk');
+
+async function compile(srcPath) {
+  const DEFAULT_COMPILER_VERSION = '0.5.0'; // <------- integrate with compile.js
+
+  const supplier = new compilerSupplier({
+    version: DEFAULT_COMPILER_VERSION
+  });
+  const solc = await supplier.load();
+  const inputDescription = JSON.stringify({
+    language: 'Solidity',
+    sources: {
+      [srcPath]: {
+        content: fs.readFileSync(srcPath, 'utf-8')
+      }
+    },
+    settings: {
+      outputSelection: {
+        '*': {
+          '': ['ast']
+        }
+      }
+    }
+  });
+  const solcOutput = solc.compile(inputDescription);
+
+  fs.writeFileSync('testast.json', solcOutput);
+
+  return JSON.parse(solcOutput);
+}
+
+function getLinearContractIds(ast, targetContract) {
+  return ast.nodes
+    .find(node => node.name == targetContract)
+    .linearizedBaseContracts.reverse();
+}
+
+function getContractNodesById(solcOutput) {
+  return Object.values(solcOutput.sources).reduce((acc, src) => {
+    src.ast.nodes
+      .filter(node => node.nodeType == 'ContractDefinition')
+      .forEach(node => (acc[node.id] = node));
+    return acc;
+  }, {});
+}
+
+function flexTable(table) {
+  table.options.head = table.options.head
+    .map(str => str.toLowerCase())
+    .map(str => chalk.cyanBright.bold(str));
+  table.options.colWidths = [];
+  table.options.chars['left-mid'] = '';
+  table.options.chars['mid'] = '';
+  table.options.chars['right-mid'] = '';
+  table.options.chars['mid-mid'] = '';
+
+  // remove long prefix 0s
+  for (var i = 0; i < table.length; i++) {
+    var value = table[i][5];
+    if (typeof value == 'string') {
+      while (value.startsWith('0x00')) {
+        value = value.replace('0x00', '0x0');
+      }
+      table[i][5] = value;
+    }
+  }
+}
+
+async function addVariableValue(storageTable, address, web3) {
+  for (let i = 0; i < storageTable.length; i++) {
+    var type = storageTable[i][1];
+    var size = storageTable[i][2];
+    var index = storageTable[i][3];
+    var startByte = storageTable[i][4];
+    var hexValue = await web3.eth.getStorageAt(address, index);
+
+    hexValue = hexValue.replace('0x', '');
+    hexValue = hexValue.padStart(64, '0');
+    var hexLen = hexValue.length;
+    var value = hexValue.slice(
+      hexLen - 2 * startByte - 2 * size,
+      hexLen - 2 * startByte
+    );
+    // hex formatting
+    if (value.indexOf('0x') == -1) {
+      value = '0x' + value;
+    }
+
+    // type converting
+    if (type.indexOf('function') == -1 && type.indexOf('mapping') == -1) {
+      if (type.indexOf('uint') != -1) {
+        value = parseInt(value, 16).toString();
+      } else if (type.indexOf('int') != -1) {
+        size = parseInt(type.split('t')[1]) / 8;
+        if (isNaN(size)) {
+          size = 32;
+        }
+        value = hexToSignedInt(value, size);
+      } else if (type == 'bool') {
+        value = parseInt(value, 16);
+        if (value == 0) {
+          value = 'false';
+        } else if (value == 1) {
+          value = 'true';
+        }
+      } else if (type == 'string') {
+        value = value.slice(0, value.length - 1);
+        var j;
+        for (j = value.length - 1; j >= 0; j--) {
+          if (value[j] != 0) {
+            break;
+          }
+        }
+        value = value.slice(0, j + 1);
+        value = hex2a(value);
+      }
+    }
+
+    storageTable[i].push(value);
+  }
+  return storageTable;
+}
+
+function hex2a(hexx) {
+  var hex = hexx.toString(); //force conversion
+  var str = '';
+  for (var i = 0; i < hex.length && hex.substr(i, 2) !== '00'; i += 2)
+    str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+  return str;
+}
+
+function hexToSignedInt(hex, size) {
+  var val = {
+    mask: 0x80 * Math.pow(2, 8 * (size - 1)),
+    sub: -0x100 * Math.pow(2, 8 * (size - 1))
+  };
+
+  var num;
+  // big int case
+  if (size >= 26) {
+    num = BigInt(hex);
+    val.mask = BigInt(val.mask);
+    val.sub = BigInt(val.sub);
+  } else {
+    num = parseInt(hex, 16);
+  }
+
+  if ((num & val.mask) != 0) {
+    //negative
+    return (val.sub + num).toString();
+  } else {
+    //positive
+    return num.toString();
+  }
+}
+
+module.exports = {
+  compile: compile,
+  getLinearContractIds: getLinearContractIds,
+  getContractNodesById: getContractNodesById,
+  flexTable: flexTable,
+  addVariableValue: addVariableValue
+};
