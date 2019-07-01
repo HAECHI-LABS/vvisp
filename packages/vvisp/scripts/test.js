1360469059760ad24bce8969e953ac1367df7ed6
wetcod <iscc3208@gmail.com>
2019-07-01 15:27:08 +0900

    fix: change exec funcs in test command to spawn for live logs

--- packages/vvisp/scripts/test.js
+++ packages/vvisp/scripts/test.js
@@ -1,5 +1,6 @@
 const { printOrSilent } = require('@haechi-labs/vvisp-utils');
-const { exec, execFile, execSync } = require('child_process');
+const { exec, execFile, execSync, spawn } = require('child_process');
+
 module.exports = async function(files, options) {
   let test_command = './node_modules/.bin/truffle test';
   let bc_command = './node_modules/.bin/ganache-cli';
@@ -24,10 +25,45 @@ module.exports = async function(files, options) {
       (error, stdout, stderr) => {}
     );
   }
-  exec(test_command, (error, stdout, stderr) => {
-    printOrSilent(stdout, options);
+
+  const cmdarr = test_command.split(' ');
+  const cmd = cmdarr[0];
+  const args = cmdarr.slice(1);
+
+  const child = spawn(cmd, args);
+  child.stdout.on('data', function(data) {
+    printOrSilent(data.toString(), options);
+  });
+
+  child.stderr.on('data', function(data) {
+    printOrSilent(data.toString(), options);
+  });
+
+  child.on('exit', function(code) {
     if (ganache_process) {
       ganache_process.kill();
     }
   });
+
+  // ls    = spawn('ls', ['-lh', '/usr']);
+
+  // ls.stdout.on('data', function (data) {
+  //   console.log('stdout: ' + data.toString());
+  // });
+
+  // ls.stderr.on('data', function (data) {
+  //   console.log('stderr: ' + data.toString());
+  // });
+
+  // ls.on('exit', function (code) {
+  //   console.log('child process exited with code ' + code.toString());
+  // });
+
+  // exec(test_command, (error, stdout, stderr) => {
+  //   printOrSilent(stdout, options);
+
+  //   if (ganache_process) {
+  //     ganache_process.kill();
+  //   }
+  // });
 };
