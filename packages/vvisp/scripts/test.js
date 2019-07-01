6ce9873665eb20820e71d8c44ae0130deac50849
wetcod <iscc3208@gmail.com>
2019-07-01 14:51:38 +0900

    feat: test arguments adoption

--- packages/vvisp/scripts/test.js
+++ packages/vvisp/scripts/test.js
@@ -1,16 +1,18 @@
 const { printOrSilent } = require('@haechi-labs/vvisp-utils');
 const { exec, execFile, execSync } = require('child_process');
-
 module.exports = async function(files, options) {
   let test_command = './node_modules/.bin/truffle test';
   let bc_command = './node_modules/.bin/ganache-cli';
   let port = 8545;
-
   if (options.coverage) {
     test_command = './node_modules/.bin/solidity-coverage';
     bc_command = './node_modules/.bin/testrpc-sc';
     port = 8555;
   }
+
+  files.forEach(file => {
+    test_command += ` ${file}`;
+  });
 
   let ganache_process;
   try {
@@ -22,10 +24,8 @@ module.exports = async function(files, options) {
       (error, stdout, stderr) => {}
     );
   }
-
   exec(test_command, (error, stdout, stderr) => {
     printOrSilent(stdout, options);
-
     if (ganache_process) {
       ganache_process.kill();
     }
